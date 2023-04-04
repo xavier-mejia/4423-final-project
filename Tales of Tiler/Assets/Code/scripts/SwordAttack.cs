13016044a512b154ffa09c08eb24040ae798925e
Xavier Mejia <90576608+xavier-mejia@users.noreply.github.com>
2023-04-03 22:45:09 -0500

    Update SwordAttack.cs
    adding sword sound effects

--- Tales of Tiler/Assets/Code/scripts/SwordAttack.cs
+++ Tales of Tiler/Assets/Code/scripts/SwordAttack.cs
@@ -7,10 +7,15 @@ public class SwordAttack : MonoBehaviour
 {
     private int _swordDamage = 10;
     private Collider2D _swordCollider;
+
+    public AudioClip hitSuccess;
+    public AudioClip hitFail;
+    private AudioSource _audioSource;
     
     void Start()
     {
         _swordCollider = GetComponent<Collider2D>();
+        _audioSource = GetComponent<AudioSource>();
     }
     
     private void OnTriggerEnter2D(Collider2D col)
@@ -18,7 +23,12 @@ public class SwordAttack : MonoBehaviour
         if (col.CompareTag("Enemy"))
         {
             Debug.Log("Enemy Hit!");
+            _audioSource.PlayOneShot(hitSuccess);
             col.GetComponent<Enemy>().TakeDamage(_swordDamage);
+        }
+        else
+        {
+            _audioSource.Play();
         }
     }
 }
