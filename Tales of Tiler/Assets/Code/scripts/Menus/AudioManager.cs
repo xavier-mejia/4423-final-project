7395e27eb49e6274bf308f734c3232ecc0aa0cf1
Xavier Mejia <90576608+xavier-mejia@users.noreply.github.com>
2023-05-03 23:32:19 -0500

    add audio options

---
+++ Tales of Tiler/Assets/Code/scripts/Menus/AudioManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("MasterVolume"))
+            mixer.SetFloat("MasterVolume", PlayerPrefs.GetFloat("MasterVolume"));
+        
+        if (PlayerPrefs.HasKey("MusicVolume"))
+            mixer.SetFloat("MusicVolume", PlayerPrefs.GetFloat("MusicVolume"));
+        
+        if (PlayerPrefs.HasKey("SfxVolume")) 
+            mixer.SetFloat("SfxVolume", PlayerPrefs.GetFloat("SfxVolume"));
+    }
+}
--- Tales of Tiler/Assets/Code/scripts/Menus/OptionsMenu.cs
+++ Tales of Tiler/Assets/Code/scripts/Menus/OptionsMenu.cs
@@ -3,63 +3,33 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Audio;
 
 public class OptionsMenu : MonoBehaviour
 {
     public Toggle fullscreen;
     public Toggle vsync;
     public List<Resolution> resolutions = new();
+    public AudioMixer mixer;
     public TMP_Text resolutionText;
+    public TMP_Text masterText, musicText, sfxText;
+    public Slider masterSlider, musicSlider, sfxSlider;
     private int _resolutionIndex;
 
-    public void Start()
+    private void Start()
     {
         fullscreen.isOn = Screen.fullScreen;
-
-        if (QualitySettings.vSyncCount == 0)
+        vsync.isOn = QualitySettings.vSyncCount == 0 ? true : false;
+        Resolution currentResolution = new Resolution
         {
-            vsync.isOn = false;
-        }
-        else
-        {
-            vsync.isOn = true;
-        }
-
-        bool hasCurrentResolution = false;
-        for (int i = 0; i < resolutions.Count(); i++)
-        {
-            if ((Screen.width == resolutions[i].width) && (Screen.height == resolutions[i].height))
-            {
-                hasCurrentResolution = true;
-                _resolutionIndex = i;
-                UpdateResolutionText();
-            }
-        }
-
-        if (!hasCurrentResolution)
-        {
-            Resolution newResolution = new Resolution()
-            {
-                width = Screen.width,
-                height = Screen.height
-            };
-            resolutions.Add(newResolution);
-            
-            /* Assuming all resolutions have a unique width & height,
-             it should be safe to compare each resolution by width only.*/
-            resolutions.Sort((res1, res2) => res1.width.CompareTo(res2.width));
-            
-            _resolutionIndex = resolutions.FindIndex(r =>
-                r.width == newResolution.width
-                && r.height == newResolution.height
-            );
-
-            UpdateResolutionText();
-            Debug.Log("Index: " + _resolutionIndex);
-        }
+            width = Screen.width,
+            height = Screen.height
+        };
+        AddResolutionIfNotPresent(currentResolution);
+        InitializeVolumeSliders();
     }
 
-    public void DecreaseResolution()
+    private void DecreaseResolution()
     {
         if (_resolutionIndex > 0)
         {
@@ -69,7 +39,7 @@ public class OptionsMenu : MonoBehaviour
         UpdateResolutionText();
     }
 
-    public void IncreaseResolution()
+    private void IncreaseResolution()
     {
         if (_resolutionIndex < resolutions.Count - 1)
         {
@@ -84,7 +54,7 @@ public class OptionsMenu : MonoBehaviour
         resolutionText.text = resolutions[_resolutionIndex].ToString();
     }
     
-    public void ApplyGraphics()
+    private void ApplyGraphics()
     {
         QualitySettings.vSyncCount = vsync.isOn ? 1 : 0;
         Screen.SetResolution(
@@ -92,6 +62,75 @@ public class OptionsMenu : MonoBehaviour
             resolutions[_resolutionIndex].height,
             fullscreen.isOn
             );
+    }
+    
+    public void SetMasterVolume()
+    {
+        masterText.text = Mathf.RoundToInt(masterSlider.value + 80).ToString();
+        mixer.SetFloat("MasterVolume", masterSlider.value);
+        PlayerPrefs.SetFloat("MasterVolume", masterSlider.value);
+    }
+    
+    public void SetMusicVolume()
+    {
+        musicText.text = Mathf.RoundToInt(musicSlider.value + 80).ToString();
+        mixer.SetFloat("MusicVolume", musicSlider.value);
+        PlayerPrefs.SetFloat("MusicVolume", musicSlider.value);
+    }
+    
+    public void SetSfxVolume()
+    {
+        sfxText.text = Mathf.RoundToInt(sfxSlider.value + 80).ToString();
+        mixer.SetFloat("SfxVolume", sfxSlider.value);
+        PlayerPrefs.SetFloat("SfxVolume", sfxSlider.value);
+    }
+
+    private void InitializeVolumeSliders()
+    {
+        float volume = 0f;
+        mixer.GetFloat("MasterVolume", out volume);
+        masterSlider.value = volume;
+        masterText.text = Mathf.RoundToInt(masterSlider.value + 80).ToString();
+
+        
+        mixer.GetFloat("MusicVolume", out volume);
+        musicSlider.value = volume;
+        musicText.text = Mathf.RoundToInt(musicSlider.value + 80).ToString();
+
+        mixer.GetFloat("SfxVolume", out volume);
+        sfxSlider.value = volume;
+        sfxText.text = Mathf.RoundToInt(sfxSlider.value + 80).ToString();
+    }
+    
+    private void AddResolutionIfNotPresent(Resolution resolution)
+    {
+        bool hasCurrentResolution = false;
+        for (int i = 0; i < resolutions.Count(); i++)
+        {
+            if ((resolution.width == resolutions[i].width) && (resolution.height == resolutions[i].height))
+            {
+                hasCurrentResolution = true;
+                _resolutionIndex = i;
+                UpdateResolutionText();
+            }
+        }
+
+        if (!hasCurrentResolution)
+        {
+            resolutions.Add(resolution);
+            
+            /* Assuming all resolutions have a unique width & height,
+             it should be safe to compare each resolution by width only.*/
+            resolutions.Sort((res1, res2) => res1.width.CompareTo(res2.width));
+            
+            _resolutionIndex = resolutions.FindIndex(r =>
+                r.width == resolution.width
+                && r.height == resolution.height
+            );
+
+            UpdateResolutionText();
+            Debug.Log("Index: " + _resolutionIndex);
+        }
     }
 }
 
