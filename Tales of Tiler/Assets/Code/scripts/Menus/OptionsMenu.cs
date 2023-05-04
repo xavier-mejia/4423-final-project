a7458cca4a045a52614a1359b903ecca09ac3d41
Xavier Mejia <90576608+xavier-mejia@users.noreply.github.com>
2023-05-03 20:13:06 -0500

    update resolution options and main menu visuals

--- Tales of Tiler/Assets/Code/scripts/Menus/OptionsMenu.cs
+++ Tales of Tiler/Assets/Code/scripts/Menus/OptionsMenu.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -7,7 +8,7 @@ public class OptionsMenu : MonoBehaviour
 {
     public Toggle fullscreen;
     public Toggle vsync;
-    public List<Resolution> resolutions = new List<Resolution>();
+    public List<Resolution> resolutions = new();
     public TMP_Text resolutionText;
     private int _resolutionIndex;
 
@@ -22,6 +23,39 @@ public class OptionsMenu : MonoBehaviour
         else
         {
             vsync.isOn = true;
+        }
+
+        bool hasCurrentResolution = false;
+        for (int i = 0; i < resolutions.Count(); i++)
+        {
+            if ((Screen.width == resolutions[i].width) && (Screen.height == resolutions[i].height))
+            {
+                hasCurrentResolution = true;
+                _resolutionIndex = i;
+                UpdateResolutionText();
+            }
+        }
+
+        if (!hasCurrentResolution)
+        {
+            Resolution newResolution = new Resolution()
+            {
+                width = Screen.width,
+                height = Screen.height
+            };
+            resolutions.Add(newResolution);
+            
+            /* Assuming all resolutions have a unique width & height,
+             it should be safe to compare each resolution by width only.*/
+            resolutions.Sort((res1, res2) => res1.width.CompareTo(res2.width));
+            
+            _resolutionIndex = resolutions.FindIndex(r =>
+                r.width == newResolution.width
+                && r.height == newResolution.height
+            );
+
+            UpdateResolutionText();
+            Debug.Log("Index: " + _resolutionIndex);
         }
     }
 
@@ -47,8 +81,7 @@ public class OptionsMenu : MonoBehaviour
 
     private void UpdateResolutionText()
     {
-        resolutionText.text = resolutions[_resolutionIndex].width.ToString() + "x" +
-                              resolutions[_resolutionIndex].height.ToString();
+        resolutionText.text = resolutions[_resolutionIndex].ToString();
     }
     
     public void ApplyGraphics()
@@ -67,4 +100,9 @@ public class Resolution
 {
     public int width;
     public int height;
+
+    public override string ToString()
+    {
+        return width + "x" + height;
+    }
 }
